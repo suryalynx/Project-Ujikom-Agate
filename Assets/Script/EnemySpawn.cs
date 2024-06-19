f417d9778ce06c171ec66cae35f4441ed95579f6
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-06-19 13:06:17 +0800

    Update EnemySpawn.cs

--- Assets/Script/EnemySpawn.cs
+++ Assets/Script/EnemySpawn.cs
@@ -5,7 +5,26 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
+    public Transform[] spawnPoint;
     public float speed = 250f;
 
-    
+    void Start(){
+        foreach(Transform spawnPoint in spawnPoint){
+            SpawnEnemy(spawnPoint);
+        }
+    }
+
+    void SpawnEnemy(Transform spawnPoint){
+        GameObject RandomEnemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        GameObject enemy = Instantiate(RandomEnemy, spawnPoint.position, spawnPoint.rotation);
+        SetupEnemy(enemy);
+    }
+
+    void SetupEnemy(GameObject enemy){
+        Rigidbody rb = enemy.GetComponent<Rigidbody>();
+        if(rb != null){
+            rb.velocity = transform.forward * speed; 
+        }
+    }
+
 }
