002b56e9ddb7cf3272198fabac4e8e7e41b0e783
Surya Herdianto Utomo <48855587+suryalynx@users.noreply.github.com>
2024-06-25 18:37:32 +0800

    Update EnemySpawn.cs

--- Assets/Script/Enemy/EnemySpawn.cs
+++ Assets/Script/Enemy/EnemySpawn.cs
@@ -4,30 +4,63 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
-    public Transform[] spawnPoint;
-    public int spawnInterval = 2;
-    public int spawnCount = 0 ;
-    public float speed = 2f;
+    public static EnemySpawn instance;
+    public GameObject[] animalPrefabs;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 2.0f;
+    public float checkpointDistance = 10.0f;
+
     private bool spawning = true;
 
-    void Start(){
-        foreach(Transform spawnPoint in spawnPoint){
-            SpawnEnemy(spawnPoint);
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        StartCoroutine(SpawnAnimals());
+    }
+
+    IEnumerator SpawnAnimals()
+    {
+        while (spawning)
+        {
+            SpawnAnimal();
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 
-    void SpawnEnemy(Transform spawnPoint){
-        GameObject RandomEnemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
-        GameObject enemy = Instantiate(RandomEnemy, spawnPoint.position, spawnPoint.rotation);
-        SetupEnemy(enemy);
-    }
-
-    void SetupEnemy(GameObject enemy){
-        Rigidbody rb = enemy.GetComponent<Rigidbody>();
-        if(rb != null){
-            rb.velocity = -transform.forward * speed; 
+    void SpawnAnimal()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        GameObject animal = Instantiate(animalPrefabs[animalIndex], spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
+        AnimalAI animalAI = animal.GetComponent<AnimalAI>();
+        if (animalAI != null)
+        {
+            animalAI.checkpoint = CalculateCheckpointPosition(spawnPoints[spawnIndex]);
         }
     }
 
+    Transform CalculateCheckpointPosition(Transform spawnPoint)
+    {
+        // Create a new GameObject to hold the checkpoint position
+        GameObject checkpointObject = new GameObject("Checkpoint");
+        checkpointObject.transform.position = spawnPoint.position + spawnPoint.forward * checkpointDistance;
+        return checkpointObject.transform;
+        
+    }
+
+    public void StopSpawning()
+    {
+        spawning = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
